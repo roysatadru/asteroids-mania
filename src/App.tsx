3cdc41cf08f3cda40560c307ec2c3ac5111d3cd8
Satadru Roy <satadru.roy98@gmail.com>
2021-10-24 01:12:53 +0530

    react router setup with /home and /lookup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,15 @@
-import { Fragment, useEffect, useState } from 'react';
-import { DateRange } from '@material-ui/pickers/DateRangePicker/RangeTypes';
+import { Redirect, Route, Switch } from 'react-router';
 
-import { axios, URIS } from './api';
-import { DateComponent } from './components/DateComponent/DateComponent';
-import { Asteroid } from './models/Asteroid';
-import { extractAsteroidInfoFromApiResponse } from './utility/mapping-info';
-import { AsteroidCardList } from './containers/AsteroidCardList';
-import { DateBasedAsteroidList } from './containers/DateBasedAsteroidList';
+import { Home } from './pages/Home';
+import { AsteroidDetails } from './pages/AsteroidDetails';
 
 export const App = () => {
-  const [asteroidsList, setAsteroidsList] = useState<Asteroid[]>([]);
-  const [[startDate, endDate], setDateRange] = useState<DateRange<Date>>([
-    null,
-    null,
-  ]);
-
-  useEffect(() => {
-    axios
-      .get<{ near_earth_objects: any[] }>(URIS.LIST_ASTEROIDS, {
-        params: {
-          page: 1,
-          size: 10,
-        },
-      })
-      .then(response => {
-        const { near_earth_objects: nearEarthObjects } = response.data;
-
-        extractAsteroidInfoFromApiResponse({
-          data: nearEarthObjects,
-          afterConversion: astList => {
-            setAsteroidsList(astList);
-          },
-        });
-      })
-      .catch(error => {});
-  }, []);
-
   return (
-    <Fragment>
-      <div>Welcome to Asteroids Mania!</div>
+    <Switch>
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/lookup/:id" component={AsteroidDetails} />
 
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          padding: '40px 32px',
-        }}
-      >
-        <DateComponent onChange={newValue => setDateRange(newValue)} />
-      </div>
-
-      {!startDate || !endDate ? (
-        <AsteroidCardList listData={asteroidsList} />
-      ) : (
-        <DateBasedAsteroidList startDate={startDate} endDate={endDate} />
-      )}
-    </Fragment>
+      <Redirect from="/" to="/home" />
+    </Switch>
   );
 };
---
+++ src/pages/AsteroidDetails.tsx
@@ -0,0 +1,3 @@
+export const AsteroidDetails = () => {
+  return <>Asteroid Details Route</>;
+};
