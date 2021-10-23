80f3ce16357500b7ec3f8ca88073657f6e058f0d
Satadru Roy <satadru.roy98@gmail.com>
2021-10-23 21:25:03 +0530

    separated conversion logic from response to a usable data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Key, useEffect, useState } from 'react';
-import isAfter from 'date-fns/isAfter';
 
 import { axios, URIS } from './api';
 import { Asteroid } from './models/Asteroid';
+import { extractAsteroidInfoFromApiResponse } from './utility/mapping-info';
 
 export const App = () => {
   const [asteroidsList, setAsteroidsList] = useState<Asteroid[]>([]);
@@ -18,58 +18,12 @@ export const App = () => {
       .then(response => {
         const { near_earth_objects: nearEarthObjects } = response.data;
 
-        const astList = nearEarthObjects.map(astInfo => {
-          const closeApproachDates: {
-            prevDate: Date | null;
-            nextDate: Date | null;
-          } = {
-            prevDate: null,
-            nextDate: null,
-          };
-
-          for (let i = 0; i < astInfo.close_approach_data.length; i++) {
-            const caDate = new Date(
-              astInfo.close_approach_data[i].close_approach_date_full,
-            );
-            const isDateAfterNow = isAfter(caDate, new Date());
-
-            if (
-              i === astInfo.close_approach_data.length - 1 &&
-              !isDateAfterNow
-            ) {
-              closeApproachDates.prevDate = caDate;
-            }
-
-            if (i === 0 && isDateAfterNow) {
-              closeApproachDates.nextDate = caDate;
-              break;
-            }
-
-            if (isDateAfterNow) {
-              closeApproachDates.prevDate = new Date(
-                astInfo.close_approach_data[i - 1].close_approach_date_full,
-              );
-              closeApproachDates.nextDate = caDate;
-              break;
-            }
-          }
-
-          return {
-            id: astInfo.id as String,
-            name: astInfo.name_limited as String,
-            isPotentiallyHazardousAsteroid:
-              astInfo.is_potentially_hazardous_asteroid as boolean,
-            estimatedDiameterInKms: {
-              min: astInfo.estimated_diameter.kilometers
-                .estimated_diameter_min as number,
-              max: astInfo.estimated_diameter.kilometers
-                .estimated_diameter_max as number,
-            },
-            closeApproachDates,
-          };
+        extractAsteroidInfoFromApiResponse({
+          data: nearEarthObjects,
+          afterConversion: astList => {
+            setAsteroidsList(astList);
+          },
         });
-
-        setAsteroidsList(astList);
       })
       .catch(error => {});
   }, []);
