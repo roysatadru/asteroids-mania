b2df1b13bdf6f15921c5fcadff1b15f9711b2441
Satadru Roy <satadru.roy98@gmail.com>
2021-10-24 18:50:53 +0530

    Added reset and theme-provider to the app

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,19 @@ import { LocalizationProvider } from '@material-ui/pickers';
 import DateFnsAdapter from '@material-ui/pickers/adapter/date-fns';
 import enLocale from 'date-fns/locale/en-US';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
 
 import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter basename="/asteroids-mania">
-      <LocalizationProvider dateAdapter={DateFnsAdapter} locale={enLocale}>
-        <App />
-      </LocalizationProvider>
+      <ThemeProvider theme={createTheme({})}>
+        <CssBaseline />
+        <LocalizationProvider dateAdapter={DateFnsAdapter} locale={enLocale}>
+          <App />
+        </LocalizationProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
